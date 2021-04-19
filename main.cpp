7c52fd3bc11777190dae67b4d39ec1ebec1141f9
mario mesen <>
2021-04-19 00:30:24 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+ cout<< "introducir dos numeros ";             
+
+    int n1, n2; //declaracion
+
+    cin >> n1 >> n2 ;
+    cout <<"la suma"<< n1 << "+" <<n2<< "es\n" <<n1+n2;
+
+    return 0;
+
+
+   
+
+ 
+}
